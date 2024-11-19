7ed4dd8ae7b7517d4e2fa90e600222a7927d5788
Adatdoki <63142175+Adatdoki@users.noreply.github.com>
2024-11-19 16:24:01 +0100

    Update Join_HTML.py
    ########### 2024.11.19 16:05:58 ################################## ADATDOKI ####
    # ZIP fájlban töltsd fel a HTML fájlokat, kicsomagolja a HTML mappába.
    # HTML mappában található összes HTML fájl keletkezési dátum alapján sorrendbe rendezése
    # és tartalmuk összefűzése. A végső fájlnév az első fájlnév elejétől a megadott
    # szó végéig tartó rész alapján generálódik, és a fájl automatikusan letöltésre kerül.
    #########1#########2#########3#########4#########5#########6#########7#########8

--- Join_HTML.py
+++ Join_HTML.py
@@ -1,20 +1,33 @@
-########### 2024.11.19 13:05:58 ################################## ADATDOKI ####
-# HTML mappában található összes HTML fájl keletkezési dátum alapján sorba rendezése
+########### 2024.11.19 16:05:58 ################################## ADATDOKI ####
+# ZIP fájlban töltsd fel a HTML fájlokat, kicsomagolja a HTML mappába.
+# HTML mappában található összes HTML fájl keletkezési dátum alapján sorrendbe rendezése
 # és tartalmuk összefűzése. A végső fájlnév az első fájlnév elejétől a megadott
 # szó végéig tartó rész alapján generálódik, és a fájl automatikusan letöltésre kerül.
 #########1#########2#########3#########4#########5#########6#########7#########8
 
 import os
+import zipfile
+from google.colab import files
 
-# Mappa, ahol a HTML fájlok találhatók
+# Kicsomagolási célmappa
 html_folder_path = '/content/HTML'
 
-# Összes HTML fájl listázása és keletkezési dátum alapján rendezése
+# ZIP fájl kicsomagolása a célmappába
+zip_file_path = '/content/html_files.zip'  # A feltöltött ZIP fájl elérési útvonala
+if not os.path.exists(html_folder_path):
+    os.makedirs(html_folder_path)  # HTML mappa létrehozása, ha nem létezik
+
+with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+    zip_ref.extractall(html_folder_path)
+
+print(f"A ZIP fájl kicsomagolva a következő mappába: {html_folder_path}")
+
+# Összes HTML fájl listázása és keletkezési dátum alapján rendezése (fordított sorrend)
 html_files = [
     (os.path.join(html_folder_path, f), os.path.getctime(os.path.join(html_folder_path, f)))
     for f in os.listdir(html_folder_path) if f.endswith('.html')
 ]
-sorted_html_files = sorted(html_files, key=lambda x: x[1])
+sorted_html_files = sorted(html_files, key=lambda x: x[1], reverse=True)  # Korábbi->Későbbi  
 
 # Az első fájl <body> előtti tartalmának kivonása
 with open(sorted_html_files[0][0], 'r', encoding='utf-8') as first_file:
@@ -52,10 +65,4 @@ with open(final_file_path, 'w', encoding='utf-8') as final_file:
     final_file.write(final_content)
 
 # Fájl letöltése
-from google.colab import files
 files.download(final_file_path)
-
-# Ha szükséges
-from IPython.display import HTML, display
-# Végső HTML tartalom megjelenítése
-display(HTML(final_content))
